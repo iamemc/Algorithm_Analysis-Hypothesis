edd04648a2f9cd5238938f926b0f262db38146a0
iamemc <38144884+iamemc@users.noreply.github.com>
2021-02-20 20:14:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -62,38 +62,3 @@ while True:
         print(str(int(value_incurve)) +" equivale a um input de "+ str(int(result_n)))
     elif choose_option == "3":
         break
-
-# =============================================================================
-# start = time.time() #start timer
-# end = time.time()
-# formatted_float = "{:.2f}".format(end - start)
-# print("\n Code took "+formatted_float+" seconds to run.")
-# =============================================================================
-
-# =============================================================================
-#exercicio 1
-#log(2º TEMPO N[1] / TEMPO N[0])
-# b = math.log(0.8/0.1,2)
-# print(b)
-# #T(N[0])/(N[0]^b)
-# a = 0.1/(1000**b)
-# print(a)
-# Tn = int(a*16000**b)
-# print(Tn)
-# =============================================================================
-
-# =============================================================================
-#exercicio 1
-#log(TEMPO N[1] / TEMPO N[0])
-# b = math.log(20/5,2)
-# #print(b)
-# a = 5/(1000**b)
-# #print(a)
-# Tn = int(a*16000**b)
-# print("16000 equivale a " + str(Tn/60) + " minutos")
-# =============================================================================
-
-#log(2º TEMPO N[1] / TEMPO N[0])
-#encontrar N
-#N = int((1.8*10**13)**(1/3))
-#print(N)
